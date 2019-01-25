f44552f42635daa2d4c874bd9917b317bac69e97
creatorOfautomation <zdp94@ukr.net>
2019-01-25 18:40:56 +0200

    completed PW6

--- PracticalWork6/src/com/alfa6/work3/Runner.java
+++ PracticalWork6/src/com/alfa6/work3/Runner.java
@@ -6,29 +6,15 @@ import java.util.Scanner;
 
 public class Runner {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public void run() {
 
         TrainSceduele trainSceduele = new TrainSceduele(2);
-        Scanner scanner = new Scanner(System.in);
         boolean wantContinue = true;
         System.out.println("U a here to create one train!!");
         while (wantContinue) {
-            System.out.println("Enter source station -->");
-            String srvStat = scanner.next();
-            System.out.println("Enter dest station -->");
-            String dstStat = scanner.next();
-            System.out.println("Enter time arrival -->");
-            String timeArr = scanner.next();
-            System.out.println("Enter time destination -->");
-            String timeDest = scanner.next();
-            System.out.println("Enter days separated by commas -->");
-            String days = scanner.next();
-            String[] daysOfWeekStr = days.split(",");
-            DaysOfWeek[] daysOfWeek = new DaysOfWeek[daysOfWeekStr.length];
-            for (int i = 0; i < daysOfWeekStr.length; i++) {
-                daysOfWeek[i] = DaysOfWeek.valueOf(daysOfWeekStr[i].toUpperCase());
-            }
-            trainSceduele.addTrain(trainSceduele.createTrain(dstStat,timeDest, timeArr, srvStat, daysOfWeek));
+            addTrainToSceduale(trainSceduele);
             System.out.println("Do you want to add one more train? [Y] or [No] -->");
             String want = scanner.next();
             if (!want.equalsIgnoreCase("Y"))
@@ -39,13 +25,37 @@ public class Runner {
         System.out.println("do u wanna find some trains in your mess? [Y] or [No] -->");
         String want = scanner.next();
         boolean wantFind = want.equalsIgnoreCase("Y");
-
         if (wantFind) {
-            System.out.println("Enter destination station --> ");
-            String dstf = scanner.next().toUpperCase();
-            System.out.println("Enter day of week --> ");
-            DaysOfWeek dowf = DaysOfWeek.valueOf(scanner.next().toUpperCase());
-            trainSceduele.searchTrains(dowf,dstf);
+            findTrains(trainSceduele);
         }
+    }
+
+    private void addTrainToSceduale(TrainSceduele trainSceduele) {
+
+        System.out.println("Enter source station -->");
+        String srvStat = scanner.next();
+        System.out.println("Enter dest station -->");
+        String dstStat = scanner.next();
+        System.out.println("Enter time arrival -->");
+        String timeArr = scanner.next();
+        System.out.println("Enter time destination -->");
+        String timeDest = scanner.next();
+        System.out.println("Enter days separated by commas -->");
+        String days = scanner.next();
+        String[] daysOfWeekStr = days.split(",");
+        DaysOfWeek[] daysOfWeek = new DaysOfWeek[daysOfWeekStr.length];
+        for (int i = 0; i < daysOfWeekStr.length; i++) {
+            daysOfWeek[i] = DaysOfWeek.valueOf(daysOfWeekStr[i].toUpperCase());
+        }
+        trainSceduele.addTrain(trainSceduele.createTrain(dstStat, timeDest, timeArr, srvStat, daysOfWeek));
+    }
+
+    private void findTrains(TrainSceduele trainSceduele) {
+
+        System.out.println("Enter destination station --> ");
+        String dstf = scanner.next().toUpperCase();
+        System.out.println("Enter day of week --> ");
+        DaysOfWeek dowf = DaysOfWeek.valueOf(scanner.next().toUpperCase());
+        trainSceduele.searchTrains(dowf, dstf);
     }
 }
