4d7569ad9e39084cef4f9bb4c8a82207f9e8e7c4
comendantOfautomation <zdp94@ukr.net>
2019-02-17 21:20:02 +0200

    Completed third task of Practical Work 13

--- PracticalWork13/src/com/alfa13/work1/Runner.java
+++ PracticalWork13/src/com/alfa13/work1/Runner.java
@@ -12,7 +12,7 @@ public class Runner {
         System.out.println("Non-sorted array " + Arrays.toString(array));
         Arrays.sort(array, (x, y) -> -x.compareTo(y));
         System.out.println("Sorted array " + Arrays.toString(array));
-        System.out.println("------------------- Strings -------------------");
+        System.out.println("---------------------- Strings ----------------------");
         List<String> strList = createStrArray();
         System.out.println("Non-sorted array " + Arrays.toString(strList.toArray()));
         String[] sortedArray = strList.toArray(new String[0]);
@@ -22,8 +22,14 @@ public class Runner {
         int sumElem = sumElem(new int[]{1, 2, 3, 4, 5, -6, -7}, integer -> integer > 0);
         System.out.println("sumElem = " + sumElem);
         printString(strList, s -> s.startsWith("B"));
-
-
+        //3
+        List<String> randomStrings = createRandomStrings();
+        System.out.println("---------------------- Convert Strings ----------------------");
+        System.out.println("String before convert");
+        System.out.println("\t" + Arrays.toString(randomStrings.toArray(new String[0])));
+        updateList(randomStrings, String::toUpperCase);
+        System.out.println("String after convert");
+        randomStrings.forEach(System.out::println);
     }
 
     public Integer[] createIntArray() {
@@ -67,6 +73,47 @@ public class Runner {
         for (String ss : s) {
             if (func.test(ss))
                 System.out.println(ss);
+        }
+    }
+
+    public void updateList(List<String> list, MyConverter<String> converter) {
+
+        ListIterator<String> iterator = list.listIterator();
+        while (iterator.hasNext())
+            iterator.set(converter.convertString(iterator.next()));
+    }
+
+    private List<String> createRandomStrings() {
+
+        List<String> list = new ArrayList<>();
+        String symbols = "qwertyuiopasdfghjklzxcvbnm";
+        for (int i = 0; i < 10; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = 0; j < 10; j++) {
+                stringBuilder.append(symbols.charAt(((int) (Math.random() * 10))));
+            }
+            list.add(stringBuilder.toString());
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("1");
+        list.add("2");
+        list.add("3");
+
+        for (String s :
+                list) {
+
+        }
+    }
+
+    private void test(List<String> strings) {
+        for (String s :
+                strings) {
+            s = s + "s";
         }
     }
 }
