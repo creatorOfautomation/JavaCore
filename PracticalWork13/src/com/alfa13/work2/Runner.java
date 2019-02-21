78f7d4f36e7694db1691f760eeb1991a6b7c14ad
comendantOfautomation <zdp94@ukr.net>
2019-02-21 17:48:44 +0200

    Refactoring

--- PracticalWork13/src/com/alfa13/work2/Runner.java
+++ PracticalWork13/src/com/alfa13/work2/Runner.java
@@ -19,15 +19,20 @@ public class Runner {
         System.out.println(integerList);
         String[] arr = new String[]{"conc", "Bonc", "ranc", "bank", "branc"};
         Stream.of(arr).filter(s -> s.startsWith("b")).sorted().forEach(System.out::println);
-
         List<Person> personList = new ArrayList<>();
-
         personList.add(new Person("John", "M", 12));
         personList.add(new Person("Linn", "F", 12));
         personList.add(new Person("Linn", "F", 14));
         personList.add(new Person("Ben", "M", 23));
-        personList.stream().filter(x -> x.getGender().equalsIgnoreCase("M") && x.getAge() >= 18 && x.getAge() < 27).forEach(System.out::println);
-        int f = (int) personList.stream().filter(x -> x.getGender().equalsIgnoreCase("F")).mapToInt(Person::getAge).average().getAsDouble();
+        personList
+                .stream()
+                .filter(x -> x.getGender().equalsIgnoreCase("M") && x.getAge() >= 18 && x.getAge() < 27)
+                .forEach(System.out::println);
+        int f = (int) personList.stream()
+                .filter(x -> x.getGender().equalsIgnoreCase("F"))
+                .mapToInt(Person::getAge)
+                .average()
+                .getAsDouble();
         System.out.println(f);
     }
 }
